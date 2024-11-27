d2fd786485ea94f16e965672f43ad776ef685e48
poomipat-k <k.poomipat@gmail.com>
2024-11-27 22:39:47 +0700

    code-writer before implement eq gt lt

--- project7/pkg/code-writer/write-arithmetic.go
+++ project7/pkg/code-writer/write-arithmetic.go
@@ -10,6 +10,14 @@ func (c *CodeWriter) WriteArithmetic(cmd string) {
 		c.writeAdd()
 	} else if cmd == "sub" {
 		c.writeSub()
+	} else if cmd == "neg" {
+		c.writeNeg()
+	} else if cmd == "and" {
+		c.writeAnd()
+	} else if cmd == "or" {
+		c.writeOr()
+	} else if cmd == "not" {
+		c.writeNot()
 	}
 }
 
@@ -36,5 +44,49 @@ func (c *CodeWriter) writeSub() {
 	_, err = c.File.WriteString("A=A-1\n")
 	check(err)
 	_, err = c.File.WriteString("M=M-D\n")
+	check(err)
+}
+
+func (c *CodeWriter) writeNeg() {
+	_, err := c.File.WriteString("@SP\n")
+	check(err)
+	_, err = c.File.WriteString("A=M-1\n")
+	check(err)
+	_, err = c.File.WriteString("M=-M\n")
+	check(err)
+}
+
+func (c *CodeWriter) writeAnd() {
+	_, err := c.File.WriteString("@SP\n")
+	check(err)
+	_, err = c.File.WriteString("AM=M-1\n")
+	check(err)
+	_, err = c.File.WriteString("D=M\n")
+	check(err)
+	_, err = c.File.WriteString("A=A-1\n")
+	check(err)
+	_, err = c.File.WriteString("M=D&M\n")
+	check(err)
+}
+
+func (c *CodeWriter) writeOr() {
+	_, err := c.File.WriteString("@SP\n")
+	check(err)
+	_, err = c.File.WriteString("AM=M-1\n")
+	check(err)
+	_, err = c.File.WriteString("D=M\n")
+	check(err)
+	_, err = c.File.WriteString("A=A-1\n")
+	check(err)
+	_, err = c.File.WriteString("M=D|M\n")
+	check(err)
+}
+
+func (c *CodeWriter) writeNot() {
+	_, err := c.File.WriteString("@SP\n")
+	check(err)
+	_, err = c.File.WriteString("A=M-1\n")
+	check(err)
+	_, err = c.File.WriteString("M=!M\n")
 	check(err)
 }
